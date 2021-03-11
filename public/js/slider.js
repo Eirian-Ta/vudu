2aedbf2ec39d3d80ed0f4d721002d8d6e4e39aac
Eirian-Ta <trang.ta911@gmail.com>
2021-03-11 17:51:46 -0500

    Responsiveness added

--- public/js/slider.js
+++ public/js/slider.js
@@ -11,20 +11,44 @@ $(document).ready(function() {
         pager: false,
         responsive : [
             {
-                breakpoint:800,
+                breakpoint:1200,
                 settings: {
-                    item:3,
-                    slideMove:1,
-                    slideMargin:6,
+                    item:7,
                   }
             },
             {
-                breakpoint:480,
+                breakpoint:1024,
+                settings: {
+                    item:6,
+                  }
+            },
+            {
+                breakpoint:900,
+                settings: {
+                    item:5,
+                  }
+            },
+            {
+                breakpoint:700,
+                settings: {
+                    item:4,
+                    //slideMove:1,
+                    //slideMargin:6,
+                  }
+            },
+            {
+                breakpoint:580,
+                settings: {
+                    item:3,
+                    //slideMove:1
+                  }
+            },
+            {
+                breakpoint:450,
                 settings: {
                     item:2,
-                    slideMove:1
                   }
-            }
+            },
         ]
     });
     $(".lSAction").children().css({ 'background-image': 'url(/images/controls.png)' });
